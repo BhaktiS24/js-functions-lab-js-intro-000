bdf9e49dd652437de09781445e9e26b5812b8afa
Bhakti Sawarkar <bhakti123.java@gmail.com>
2018-08-10 01:01:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+function happyHolidays(string)
+{
+  return "Happy Holidays!"
+}
+
+function happyHolidaysTo(name)
+{
+  return "HAPPY HOLIDAYS, ${name}"
+}
+
+function happyHolidayTo(holiday ,name)
+{
+  return 'happy ${holiday},${name}'
+}
+
+function holidayCountdown(holiday, days)
+{
+  return "Its ${days} until ${holiday} "
+}
